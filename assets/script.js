65c3bfbd6390d1af1969ec22326bc09fe1809ef0
Matthew M. Provost <provostmat@gmail.com>
2023-04-19 19:54:18 -0400

    created api call function to get the data based on city name

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,67 @@
 var searchFormEl = document.querySelector('#search-form');
+var weatherContainerEl = document.querySelector('#weather-container');
+var resultTextEl = document.querySelector('#result-text');
+var cityInputEl = document.querySelector('#search-input');
+apiKey = '21adf659df66103262a537b3b37fad51';
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
+  var cityName = cityInputEl.value.trim();
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
+  if (cityName) {
+    getGeo(cityName);
+
+    resultTextEl.textContent = '';
+    cityInputEl.value = '';
+
+  } else {
+    alert('Please enter a city');
   }
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+};
 
-  location.assign(queryString);
-}
+var getGeo = function (city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city + '&appid=' + apiKey;
+
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                displayWeather(data, city);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function (error) {
+        alert('Unable to get your weather based on city name');
+    });
+};
+
+// var getWeather = function (lat, lon) {
+//     var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+// }
+
+// var displayWeather = function(days, searchTerm) {
+//     if (days.length ===0) {
+//         weatherContainerEl.textContent = 'No weather data found for the next 5 days.';
+//         return;
+//     }
+
+//     resultTextEl.textContent = searchTerm;
+
+//     for (var i = 0; i < 5; i++) {
+        
+//         var weatherDate = days[i].
+//         var weatherEl = document.createElement('div');
+//         weatherEl.classList = 'list-item flex-row justify-space-between align-center';
+
+//         var dateEl = document.createElement('span');
+//         dateEl.textContent = 
+//     }
+// }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
